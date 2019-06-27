fa9f61a8e71f6447e8cda350e42ea143dc848698
MicroHappy <kostja.silver.men@gmail.com>
2019-06-27 17:11:15 +0400

    test

--- controllers/review.go
+++ controllers/review.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/syyongx/php2go"
 	"review-go/models"
 )
 
@@ -12,22 +13,40 @@ type ReviewController struct {
 }
 
 // @Description get all reviews
-// @Param filter query []string "Filter criterion"
 // @Param order query []string "Order criterion"
 // @Param limit query int 100 "Limit"
 // @Param offset query int 0 "Offset"
 // @Success 200 {object} models.Review
 // @router / [get]
 func (r *ReviewController) GetAll() {
-	filters := r.GetStrings("filter[]")
-	orders := r.GetStrings("order[]")
 	limit, _ := r.GetInt("limit", 100)
 	offset, _ := r.GetInt("offset", 0)
 
-	fmt.Println("params ", filters, orders, limit, offset)
+	query := "filter%5Bfirst%5D=test&filter%5Bfourth%5D%5B%5D=123&filter%5Bfourth%5D%5B%5D=456&filter%5Bsecond%5D=hello&filter%5Bthird%5D=123&limit=t&offset=w"
+
+	result := make(map[string]interface{})
+	php2go.ParseStr(query, result)
+	fmt.Println(result["filter"])
+
+	//type column struct{
+	//	Name string
+	//	Data string
+	//}
+	//columns := make(map[string]string)
+	//
+	//if err := r.Ctx.Input.Bind(&columns, "filter"); err != nil {
+	//	fmt.Println(err)
+	//} else {
+	//	fmt.Println("OK, got:", columns)
+	//}
 
 	reviews := models.GetAll(offset, limit)
 
 	r.Data["json"] = reviews
+	r.ServeJSON()
+}
+
+func (r *ReviewController) ReviewController() {
+	r.Data["json"] = "test"
 	r.ServeJSON()
 }
--- models/review.go
+++ models/review.go
@@ -36,6 +36,10 @@ func GetAll(offset int, limit int) []*Review {
 
 	fmt.Println(num)
 
+	for _, el := range reviews {
+		o.LoadRelated(el, "Photo", false, 0)
+	}
+
 	return reviews
 }
 
